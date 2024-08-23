5c0963dc05db41e10afd7e8865dbc69311bf7a53
Guilherme <domiuaunoob@outlook.com>
2024-08-23 14:14:35 -0300

    Initial Commit

---
+++ Challenge2/Challenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2App.swift
+//  Challenge2
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
